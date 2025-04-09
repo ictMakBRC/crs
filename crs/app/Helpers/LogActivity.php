abfbbc26e9cab6a7642f2c77a362c3b84448ac7b
ictMakBRC <110599300+ictMakBRC@users.noreply.github.com>
2025-04-09 23:26:20 +0300

    Update LogActivity.php

--- crs/app/Helpers/LogActivity.php
+++ crs/app/Helpers/LogActivity.php
@@ -8,7 +8,7 @@ use App\Models\ActivityLog as LogActivityModel;;
 
 class LogActivity
 {
-    public static function addToLog($description,$email,$ip)
+    public static function addToLogOld($description,$email,$ip)
     {   
     	$log = [];
         $platform=Agent::platform();
@@ -24,6 +24,38 @@ class LogActivity
     	$log['client_ip'] = $ip;
     	LogActivityModel::create($log);
     }
+
+    public static function addToLog($description, $email, $ip)
+{
+    $log = [];
+    $platform = Agent::platform();
+    $browser = Agent::browser();
+    
+    // Cross-platform MAC address solution
+    $macAddr = 'unknown';
+    try {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            // Windows
+            $macAddr = shell_exec('getmac');
+        } else {
+            // Linux/Unix/Mac
+            $macAddr = shell_exec("netstat -ie | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'");
+        }
+    } catch (\Exception $e) {
+        // Fallback if MAC address cannot be retrieved
+        $macAddr = 'unavailable';
+    }
+
+    $log['user_id'] = auth()->check() ? auth()->user()->id : null;
+    $log['email'] = $email;
+    $log['description'] = $description;
+    $log['platform'] = $platform;
+    $log['browser'] = $browser;
+    $log['mac'] = $macAddr;
+    $log['client_ip'] = $ip;
+    
+    LogActivityModel::create($log);
+}
 
     public static function logActivityLists()
     {
